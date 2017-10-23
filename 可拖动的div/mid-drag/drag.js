730867adc9c10ab9ff9cb10a3d9815b9ca93521a
hackerwen <1007326924@qq.com>
2017-10-23 19:55:21 +0800

    '可拖动的div封装版'

---
+++ 可拖动的div/mid-drag/drag.js
@@ -0,0 +1,43 @@
+var dropManager = (function() {
+	var diffX = 0, // 鼠标点击处距离盒子左侧距离
+		diffY = 0, // 鼠标点击处距离盒子顶部距离
+		dragging = null;
+
+	function handle(event) {
+		var target = event.target,
+			className = target.className,
+			type = event.type;
+		switch (type) {
+			case 'mousedown':
+				if (className.indexOf('draggable') !== -1) {
+					dragging = target;
+					diffX = event.clientX - dragging.offsetLeft;
+					diffY = event.clientY - dragging.offsetTop;
+				}
+				break;
+			case 'mousemove':
+				if (dragging !== null) {
+					dragging.style.top = event.clientY - diffY + 'px';
+					dragging.style.left = event.clientX - diffX + 'px';
+				}
+				break;
+			case 'mouseup':
+				dragging = null;
+				break;
+			default:
+				return;
+		}
+	}
+	return {
+		dragEnable: function() {
+			document.addEventListener('mousedown', handle);
+			document.addEventListener('mousemove', handle);
+			document.addEventListener('mouseup', handle);
+		},
+		dragDisable: function() {
+			document.removeEventListener('mousedown', handle);
+			document.removeEventListener('mousemove', handle);
+			document.removeEventListener('mouseup', handle);
+		}
+	}
+})()
