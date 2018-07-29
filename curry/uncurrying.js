11a78bada78d61bf81b7472709b50ee526c862b2
haiqiu <1007326924@qq.com>
2018-07-29 18:17:24 +0800

    [update]throttle debonunce curry uncurry

---
+++ curry/uncurrying.js
@@ -0,0 +1,20 @@
+Function.prototype.uncurrying = function() {
+  var self = this
+  return function() {
+    var obj = Array.prototype.shift.call(arguments)
+    return self.apply(obj, arguments)
+  }
+}
+
+// Function.prototype.uncurrying = function() {
+//   var self = this
+//   return function() {
+//     return Function.prototype.call.apply(self, arguments)
+//   }
+// }
+
+var push = Array.prototype.push.uncurrying()
+
+var obj = { 0: 0, 1: 1, length: 2 }
+push(obj, 2, 3)
+console.log(obj)
